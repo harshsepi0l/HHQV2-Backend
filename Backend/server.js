aa03ab84b7d6ae4ce65c3c8fd7abea0a9a87a604
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2023-10-17 23:06:18 -0400

    Added Frontend and Express on Backend
    Added multiple changes and created new folders. Might have to install things but npm start on both folders should work.

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,7 @@ app.get("/courses", async (req, res) => {
     const courses = await prisma.course.findMany();
     res.json(courses);
   } catch (error) {
+    console.error("Error fetching courses:", error);
     res.status(500).json({ error: "Database error" });
   }
 });
---
+++ frontend/src/CourseTable.tsx
@@ -0,0 +1,56 @@
+// CourseTable.tsx
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+type Course = {
+  course_id: string;
+  title: string;
+  credit_type: string;
+  description: string;
+  // ... other fields
+};
+
+const CourseTable: React.FC = () => {
+  const [courses, setCourses] = useState<Course[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/courses");
+        setCourses(response.data);
+      } catch (error) {
+        console.error("Failed to fetch courses:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Credit Type</th>
+          <th>Description</th>
+          {/* ... other headers */}
+        </tr>
+      </thead>
+      <tbody>
+        {courses.map((course) => (
+          <tr key={course.course_id}>
+            <td>{course.course_id}</td>
+            <td>{course.title}</td>
+            <td>{course.credit_type}</td>
+            <td>{course.description}</td>
+            {/* ... other fields */}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default CourseTable;
