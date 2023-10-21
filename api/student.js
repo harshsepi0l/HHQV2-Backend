ba752c7a7523ed226449137d0fdeabdae6048dc8
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2023-10-21 10:36:12 -0400

    changes

--- api/student.js
+++ api/student.js
@@ -2,6 +2,16 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = async (req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle OPTIONS method (important for preflight)
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   try {
     const students = await prisma.student.findMany();
     res.json(students);
