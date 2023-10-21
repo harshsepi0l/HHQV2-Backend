a0bccc4e8cc00e0eff479da3c87f867df7784146
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2023-10-21 00:04:39 -0400

    Changes

--- Backend/server.js
+++ Backend/server.js
@@ -17,6 +17,16 @@ app.get("/courses", async (req, res) => {
   }
 });
 
+app.get("/student", async (req, res) => {
+  try {
+    const students = await prisma.student.findMany();
+    res.json(students);
+  } catch (error) {
+    console.error("Error fetching students:", error);
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
 app.listen(3001, () => {
   console.log("Server is running on http://localhost:3001");
 });
