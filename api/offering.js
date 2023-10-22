b25c26eb1461e85620a1bda07b93681fc23d2cda
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2023-10-22 15:33:47 -0400

    Create offering.js

---
+++ api/offering.js
@@ -0,0 +1,22 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+module.exports = async (req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle OPTIONS method (important for preflight)
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  try {
+    const offerings = await prisma.offering.findMany();
+    res.json(offerings);
+  } catch (error) {
+    console.error("Error fetching students:", error);
+    res.status(500).json({ error: "Database error" });
+  }
+};
