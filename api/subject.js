f758359c07b925dc1d10c7584a5e9ffde29b6c5e
Harsha Ajjarapu <Harsha.ajjarapu@gmail.com>
2023-11-01 19:02:43 -0400

    Create subject.js

---
+++ api/subject.js
@@ -0,0 +1,34 @@
+/*
+API ENDPOINT FOR DEPARTMENT TABLE
+Holds:
+department
+-- course (foreign key)
+*/
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+module.exports = async (req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle OPTIONS method (important for preflight)
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  try {
+    const subjects = await prisma.subject.findMany({
+      include: {
+        course: true, //"Foreign Key" or reference we gave to the table
+      },
+    });
+
+    res.json(subjects);
+  } catch (error) {
+    console.error("Error fetching subjects:", error);
+    res.status(500).json({ error: "Database error" });
+  }
+};
