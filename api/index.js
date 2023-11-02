01ffb8b08b1d356465591a8c971f475ec5ae5464
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2023-11-02 13:53:03 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,6 @@ module.exports = (req, res) => {
   res
     .status(200)
     .send(
-      "API Base Endpoint/Or you just typed in the wrong api link :/ \n here are all the API Links: \n https://hhqv2backend.vercel.app/api/department \n https://hhqv2backend.vercel.app/api/subject \n https://hhqv2backend.vercel.app/api/division \n https://hhqv2backend.vercel.app/api/term \n https://hhqv2backend.vercel.app/api/course \n https://hhqv2backend.vercel.app/api/offering \n https://hhqv2backend.vercel.app/api/student \n https://hhqv2backend.vercel.app/api/level"
+      "Welcome to the API Index :/ \n here are all the API Links: \n https://hhqv2backend.vercel.app/api/department \n https://hhqv2backend.vercel.app/api/subject \n https://hhqv2backend.vercel.app/api/division \n https://hhqv2backend.vercel.app/api/term \n https://hhqv2backend.vercel.app/api/course \n https://hhqv2backend.vercel.app/api/offering \n https://hhqv2backend.vercel.app/api/student \n https://hhqv2backend.vercel.app/api/level"
     );
 };
