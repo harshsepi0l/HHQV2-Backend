08abba271f995b984023dcaac5b99b0cecb4d619
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2023-11-01 22:53:02 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
 module.exports = (req, res) => {
-  res.status(200).send("API Base Endpoint");
+  res
+    .status(200)
+    .send("API Base Endpoint/Or you just typed in the wrong api link :/");
 };
