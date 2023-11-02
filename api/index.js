d1e34997e0917384d56ef669a1f78225d06806f4
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2023-11-02 13:55:00 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 module.exports = (req, res) => {
-  res
-    .status(200)
-    .send(
-      "Welcome to the API Index :/ \n here are all the API Links: \n https://hhqv2backend.vercel.app/api/department \n https://hhqv2backend.vercel.app/api/subject \n https://hhqv2backend.vercel.app/api/division \n https://hhqv2backend.vercel.app/api/term \n https://hhqv2backend.vercel.app/api/course \n https://hhqv2backend.vercel.app/api/offering \n https://hhqv2backend.vercel.app/api/student \n https://hhqv2backend.vercel.app/api/level"
-    );
+  const apiBaseURL = "https://hhqv2backend.vercel.app/api";
+  const endpoints = {
+    message: "Welcome to the API Index.",
+    links: {
+      department: `${apiBaseURL}/department`,
+      subject: `${apiBaseURL}/subject`,
+      division: `${apiBaseURL}/division`,
+      term: `${apiBaseURL}/term`,
+      course: `${apiBaseURL}/course`,
+      offering: `${apiBaseURL}/offering`,
+      student: `${apiBaseURL}/student`,
+      level: `${apiBaseURL}/level`,
+    },
+  };
+
+  res.status(200).json(endpoints);
 };
