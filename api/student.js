f5b2b175f0834c1961737f97db5045d82e490840
Harsha Ajjarapu <Harsha.ajjarapu@gmail.com>
2023-11-13 00:29:22 -0500

    Update student.js

--- api/student.js
+++ api/student.js
@@ -19,15 +19,43 @@ module.exports = async (req, res) => {
     return res.status(200).end();
   }
 
-  try {
-    const students = await prisma.student.findMany({
-      include: {
-        enrollment: true, //"Foreign Key" or reference we gave to the table
-      },
-    });
-    res.json(students);
-  } catch (error) {
-    console.error("Error fetching students:", error);
-    res.status(500).json({ error: "Database error" });
+  // Handle POST request
+  if (req.method === "POST") {
+    try {
+      // Parse the incoming data
+      const studentData = JSON.parse(req.body);
+
+      // Add validation if necessary
+
+      // Create a new student in the database
+      const createdStudent = await prisma.student.create({
+        data: studentData,
+      });
+
+      res.status(201).json(createdStudent);
+    } catch (error) {
+      console.error("Error handling POST request:", error);
+      res.status(500).json({ error: "Error processing request" });
+    }
+  }
+
+  // Handle GET request
+  else if (req.method === "GET") {
+    try {
+      const students = await prisma.student.findMany({
+        include: {
+          enrollment: true, // Include related enrollments
+        },
+      });
+      res.json(students);
+    } catch (error) {
+      console.error("Error fetching students:", error);
+      res.status(500).json({ error: "Database error" });
+    }
+  }
+
+  // Handle other methods (PUT, DELETE, etc.) or send a Method Not Allowed response
+  else {
+    res.status(405).json({ error: "Method Not Allowed" });
   }
 };
